468729a3d93c48ff63b8800460bfe3674a0d4cdf
Zamtakk <28710885+Zamtakk@users.noreply.github.com>
2020-11-25 21:13:11 +0100

    Moved socket server to seperate file

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,29 @@
+#include "socket_server.hpp"
 #include <iostream>
-#include <stdlib.h>
+#include <thread>
+#include <string>
+#include <queue>
 
-int main(){
-    std::cout << "Test" << std::endl;
+using namespace std;
+
+int main()
+{
+    queue<string> *socket_rx = new queue<string>();
+    queue<string> *socket_tx = new queue<string>();
+
+    setRxTx(socket_rx, socket_tx);
+
+    // Constructs the new thread and runs it. Does not block execution.
+    thread t1(StartSocket);
+
+    while(true){
+        if(socket_rx->size() > 0){
+            string message = socket_rx->front();
+            socket_rx->pop();
+            cout << "Incoming message: " << message << endl;
+            if (message.find("exit") != string::npos) break;
+        }else{
+            this_thread::sleep_for (chrono::milliseconds(100));
+        }
+    }
 }
