da115facbed824eb32399146d29f9f5e45cd0630
Zamtakk <28710885+Zamtakk@users.noreply.github.com>
2020-12-12 10:49:00 +0100

    Deleted WebsocketConnection and DeviceRegistration class files

--- src/Websocket/DeviceRegistration.cpp
+++
@@ -1,9 +0,0 @@
-// #include "Websocket/DeviceRegistration.hpp"
-
-// DeviceRegistration::DeviceRegistration(std::string uuid, WebsocketConnection *connection): UUID(uuid), Connection(connection){
-
-// }
-
-// DeviceRegistration::~DeviceRegistration(){
-    
-// }
--- src/Websocket/WebsocketConnection.cpp
+++
@@ -1,9 +0,0 @@
-// #include "Websocket/WebsocketConnection.hpp"
-
-// WebsocketConnection::WebsocketConnection(websocketpp::connection_hdl hdl, message_ptr msg): Handle(hdl), MessagePointer(msg){
-
-// }
-
-// WebsocketConnection::~WebsocketConnection(){
-
-// }
