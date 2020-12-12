8fc412374d9a4926de43174b49f3a32482573a1a
Zamtakk <28710885+Zamtakk@users.noreply.github.com>
2020-12-12 20:20:33 +0100

    Created main code that can create device objects and pass on messages.

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,22 @@
 #include "Websocket/SocketServer.hpp"
+#include "Devices/Device.hpp"
 #include "Devices/ExampleDevice.hpp"
+#include "Devices/DeviceTypes.hpp"
+
+#include "json.hpp"
 
 #include <iostream>
 #include <thread>
 #include <string>
 
+using json = nlohmann::json;
+
 using std::cout;
 using std::endl;
+using std::map;
+using std::pair;
 using std::string;
+using std::vector;
 using std::chrono::milliseconds;
 using std::this_thread::sleep_for;
 
@@ -15,19 +24,37 @@ int main()
 {
     SocketServer *Socket = SocketServer::Instance();
     string message;
+    map<string, Device *> devices;
+    vector<ExampleDevice *> exampleDevices;
 
     while (true)
     {
         message = Socket->GetMessage();
-        if (message != "")
+        if (message == "")
         {
-            cout << "Incoming message: " << message << endl;
-            if (message.find("exit") != string::npos)
-                break;
+            // When no messages are available, wait a bit to prevent overloading the system.
+            sleep_for(milliseconds(50));
+            continue;
+        }
+
+        json jsonMessage = json::parse(message);
+
+        if (jsonMessage["command"] == REGISTRATION)
+        {
+            string newUUID, newType;
+
+            if (jsonMessage["Type"] == EXAMPLEDEVICE)
+            {
+                newUUID = jsonMessage["UUID"];
+                newType = jsonMessage["Type"];
+                ExampleDevice *newDevice = new ExampleDevice(newUUID, newType);
+                exampleDevices.push_back(newDevice);
+                devices.insert(pair<string, Device *>(newUUID, newDevice));
+            }
         }
         else
         {
-            sleep_for(milliseconds(100));
+            devices.find(jsonMessage["UUID"])->second->handleMessage(message);
         }
     }
 }
