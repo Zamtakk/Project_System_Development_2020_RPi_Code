88c797db2d9093b0a513d0041c4140acf0348e56
Corwin96 <33621431+Corwin96@users.noreply.github.com>
2020-12-17 13:14:24 +0100

    Make use of json format for sending a request

--- Web code/communication.js
+++ Web code/communication.js
@@ -1,37 +1,23 @@
-var pjsdvSocket = new WebSocket("ws://169.254.41.114:9002"); //change the ip address to the ip address of your pi! - also works when using live server
+var socket = new WebSocket("ws://169.254.41.114:9002"); //change the ip address to the ip address of your pi! - also works when using live server
 
 function log(logstr) {
 	document.getElementById("log").innerHTML += logstr + "\n";
 }
 
-async function sendSocketTest() {
-	var msg = document.getElementById("texttosend").value;
-
-	pjsdvSocket.send(msg);
-
-	document.getElementById("texttosend").value = "";
+async function sendSocket(UUID, type, command, value) {
+	var msg = {
+		UUID: 		UUID,
+		Type: 		type,
+		command:	command,
+		value: 		value
+	};
+	socket.send(JSON.stringify(msg, null, 2));
 }
 
-pjsdvSocket.onopen = function (event) {
-	pjsdvSocket.send("Here's some text that the server is urgently awaiting!");
+socket.onopen = function (event) {
+	socket.send("Here's some text that the server is urgently awaiting!");
 };
 
-pjsdvSocket.onmessage = function (event) {
+socket.onmessage = function (event) {
 	log(event.data);
-}
-
-function setStatus() {
-	var toggleswitch = document.getElementById("switchstatus");
-	var doortoggleswitch = document.getElementById("switchdoorstatus");
-	if (toggleswitch.checked == true) {
-		document.getElementById("statusvalue").innerHTML = "open";
-	} else {
-		document.getElementById("statusvalue").innerHTML = "dicht";
-	}
-
-	if (doortoggleswitch.checked == true) {
-		document.getElementById("doorstatusvalue").innerHTML = "open";
-	} else {
-		document.getElementById("doorstatusvalue").innerHTML = "dicht";
-	}
 }
