d7f7cb4ef3199af2bb01f701e6ed1dc8939a9a40
Zamtakk <28710885+Zamtakk@users.noreply.github.com>
2021-01-19 20:49:05 +0100

    Clean up main and remove example device

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,27 @@
+// Includes
+
 #include "Websocket/SocketServer.hpp"
-#include "Devices/Device.hpp"
-#include "Devices/ExampleDevice.hpp"
-#include "Devices/Chair.hpp"
-#include "Devices/Website.hpp"
-#include "Devices/SimulatedDevice.hpp"
+
 #include "Devices/Bed.hpp"
+#include "Devices/Chair.hpp"
+#include "Devices/Column.hpp"
+#include "Devices/Device.hpp"
 #include "Devices/Door.hpp"
-#include "Devices/Wall.hpp"
-#include "Devices/Lamp.hpp"
 #include "Devices/Fridge.hpp"
+#include "Devices/Lamp.hpp"
+#include "Devices/SimulatedDevice.hpp"
+#include "Devices/Wall.hpp"
+#include "Devices/Website.hpp"
 #include "Devices/WIB.hpp"
 
 #include "json.hpp"
-
 #include <iostream>
 #include <thread>
 #include <string>
 
-using json = nlohmann::json;
+// Define namespace functions
 
+using nlohmann::json;
 using std::cout;
 using std::endl;
 using std::map;
@@ -29,6 +32,8 @@ using std::vector;
 using std::chrono::milliseconds;
 using std::this_thread::sleep_for;
 
+// Main loop
+
 int main()
 {
     SocketServer *Socket = SocketServer::Instance();
@@ -37,6 +42,7 @@ int main()
 
     while (true)
     {
+        // Get new messages from socket server
         message = Socket->GetMessage();
         if (message == "")
         {
@@ -45,85 +51,59 @@ int main()
             continue;
         }
 
+        // Parse the message to JSON so it is easier to read.
         json jsonMessage = json::parse(message);
 
+        // If it is a registration message, create a new device of that type
         if (jsonMessage["command"] == REGISTRATION)
         {
-            string newUUID, newType;
+            string newUUID = jsonMessage["UUID"];
+            string newType = jsonMessage["Type"];
 
-            if (jsonMessage["Type"] == "ExampleDevice")
+            if (jsonMessage["Type"] == "Bed")
             {
-                newUUID = jsonMessage["UUID"];
-                newType = jsonMessage["Type"];
-                ExampleDevice *newDevice = new ExampleDevice(newUUID, newType, Socket, &devices);
-                devices.insert(pair<string, Device *>(newUUID, newDevice));
+                devices.insert(pair<string, Device *>(newUUID, new Bed(newUUID, newType, Socket, &devices)));
             }
             else if (jsonMessage["Type"] == "Chair")
             {
-                newUUID = jsonMessage["UUID"];
-                newType = jsonMessage["Type"];
-                Chair *newDevice = new Chair(newUUID, newType, Socket, &devices);
-                devices.insert(pair<string, Device *>(newUUID, newDevice));
+                devices.insert(pair<string, Device *>(newUUID, new Chair(newUUID, newType, Socket, &devices)));
             }
-            else if (jsonMessage["Type"] == "Website")
+            else if (jsonMessage["Type"] == "Column")
             {
-                newUUID = jsonMessage["UUID"];
-                newType = jsonMessage["Type"];
-                Website *newDevice = new Website(newUUID, newType, Socket, &devices);
-                devices.insert(pair<string, Device *>(newUUID, newDevice));
-            }
-            else if (jsonMessage["Type"] == "SimulatedDevice")
-            {
-                newUUID = jsonMessage["UUID"];
-                newType = jsonMessage["Type"];
-                SimulatedDevice *newDevice = new SimulatedDevice(newUUID, newType, Socket, &devices);
-                devices.insert(pair<string, Device *>(newUUID, newDevice));
-            }
-            else if (jsonMessage["Type"] == "Bed")
-            {
-                newUUID = jsonMessage["UUID"];
-                newType = jsonMessage["Type"];
-                Bed *newDevice = new Bed(newUUID, newType, Socket, &devices);
-                devices.insert(pair<string, Device *>(newUUID, newDevice));
+                devices.insert(pair<string, Device *>(newUUID, new Column(newUUID, newType, Socket, &devices)));
             }
             else if (jsonMessage["Type"] == "Door")
             {
-                newUUID = jsonMessage["UUID"];
-                newType = jsonMessage["Type"];
-                Door *newDevice = new Door(newUUID, newType, Socket, &devices);
-                devices.insert(pair<string, Device *>(newUUID, newDevice));
-            }
-            else if (jsonMessage["Type"] == "Wall")
-            {
-                newUUID = jsonMessage["UUID"];
-                newType = jsonMessage["Type"];
-                Wall *newDevice = new Wall(newUUID, newType, Socket, &devices);
-                devices.insert(pair<string, Device *>(newUUID, newDevice));
-            }
-            else if (jsonMessage["Type"] == "Lamp")
-            {
-                newUUID = jsonMessage["UUID"];
-                newType = jsonMessage["Type"];
-                Lamp *newDevice = new Lamp(newUUID, newType, Socket, &devices);
-                devices.insert(pair<string, Device *>(newUUID, newDevice));
+                devices.insert(pair<string, Device *>(newUUID, new Door(newUUID, newType, Socket, &devices)));
             }
             else if (jsonMessage["Type"] == "Fridge")
             {
-                newUUID = jsonMessage["UUID"];
-                newType = jsonMessage["Type"];
-                Fridge *newDevice = new Fridge(newUUID, newType, Socket, &devices);
-                devices.insert(pair<string, Device *>(newUUID, newDevice));
+                devices.insert(pair<string, Device *>(newUUID, new Fridge(newUUID, newType, Socket, &devices)));
+            }
+            else if (jsonMessage["Type"] == "Lamp")
+            {
+                devices.insert(pair<string, Device *>(newUUID, new Lamp(newUUID, newType, Socket, &devices)));
+            }
+            else if (jsonMessage["Type"] == "SimulatedDevice")
+            {
+                devices.insert(pair<string, Device *>(newUUID, new SimulatedDevice(newUUID, newType, Socket, &devices)));
+            }
+            else if (jsonMessage["Type"] == "Wall")
+            {
+                devices.insert(pair<string, Device *>(newUUID, new Wall(newUUID, newType, Socket, &devices)));
+            }
+            else if (jsonMessage["Type"] == "Website")
+            {
+                devices.insert(pair<string, Device *>(newUUID, new Website(newUUID, newType, Socket, &devices)));
             }
             else if (jsonMessage["Type"] == "WIB")
             {
-                newUUID = jsonMessage["UUID"];
-                newType = jsonMessage["Type"];
-                WIB *newDevice = new WIB(newUUID, newType, Socket, &devices);
-                devices.insert(pair<string, Device *>(newUUID, newDevice));
+                devices.insert(pair<string, Device *>(newUUID, new WIB(newUUID, newType, Socket, &devices)));
             }
         }
         else
         {
+            // If it is a normal message, check if the device exists and pass on the message
             if (devices.count(jsonMessage["UUID"]) != 0)
             {
                 devices.find(jsonMessage["UUID"])->second->HandleMessage(message);
